a40793af64708f5e3f323907bf40a166797842e0
David de Prez <dosprez@avans.nl>
2017-04-11 18:11:09 +0200

    3D Ball
    ADD		3D effect to ball
    ADD		difference between 2 screens

--- sources_1/new/ball.vhd
+++ sources_1/new/ball.vhd
@@ -43,24 +43,24 @@ entity ball is
 end ball;
 
 architecture Behavioral of ball is
-    SIGNAL X_bal, Y_bal : STD_LOGIC_VECTOR(9 downto 0) := std_logic_vector(to_signed(300, 10)); --"0100101100";
-    SIGNAL size_bal : integer := 80; -- "0001100011";
+    SIGNAL X_bal, Y_bal : integer := 100; --"0100101100";
+    SIGNAL size_bal : integer := 800; -- "0001100011";
     SIGNAL z_index : integer := 5;
 begin
 
 mover: process(clk25)
-    variable prescaler : integer := 0;
+    variable prescaler : integer := -1000;
     variable dir : boolean := true;
 begin
     if (rising_edge(clk25)) then
         prescaler := prescaler + 1;
                 
-        if (prescaler >= 25000000) then
+        if (prescaler >= 3000000) then
             prescaler := 0;
             
-            if (z_index > 300) then
+            if (z_index > -2) then
                 dir := false;
-            elsif (z_index < -300) then
+            elsif (z_index < -20) then
                 dir := true;
             end if;
         
@@ -75,39 +75,57 @@ end process;
 
 
 main: process(clk25)
-    variable a2, b2, c2 : integer;
-    variable xMem, yMem : integer;
+    variable a2_left, b2_left, c2_left : integer;
+    variable a2_right, b2_right, c2_right : integer;
+    
+    variable xMem_left, yMem_left : integer;
+    variable xMem_right, yMem_right : integer;
+    
     variable X_center, Y_center : integer;
 begin  
     if (rising_edge(clk25)) then
-        X_center := to_integer(signed(X + 320));
-        Y_center := to_integer(signed(Y + 240));
+        X_center := to_integer(signed(X - 320));
+        Y_center := to_integer(signed(Y - 240));
     
-        xMem := X_center * z_index;
-        yMem := Y_center * z_index;
+        xMem_left := X_center * z_index;
+        yMem_left := Y_center * z_index;
+        
+        xMem_right := X_center * (-22 - z_index);
+        yMem_right := Y_center * (-22 - z_index);
         
 --        a2 := to_integer(signed(X_bal)) - xMem;
 --        b2 := to_integer(signed(Y_bal)) - yMem;
 
-        a2 := to_integer(signed(X_bal - xMem));
-        b2 := to_integer(signed(Y_bal - yMem));
-        a2 := a2**2;
-        b2 := b2**2;
-        c2 := a2 + b2;
+        a2_right := xMem_right - X_bal;
+        b2_right := yMem_right - Y_bal;
+        a2_right := a2_right**2;
+        b2_right := b2_right**2;
+        c2_right := a2_right + b2_right;
         
-        if(c2 <= size_bal) then
+        a2_left := xMem_left - X_bal;
+        b2_left := yMem_left - Y_bal;
+        a2_left := a2_left**2;
+        b2_left := b2_left**2;
+        c2_left := a2_left + b2_left;
+        
+        if(c2_left <= size_bal) then
                 out_left(11 downto 8) <= "0000";
                 out_left(7 downto 4) <= "0000";
                 out_left(3 downto 0) <= "1111";
+                empty_left <= '0';
+        else
+                empty_left <= '1'; 
+        end if;
+        
+        if(c2_right <= size_bal) then
                 out_right(11 downto 8) <= "0000";
                 out_right(7 downto 4) <= "1111";
                 out_right(3 downto 0) <= "0000";
-                empty_left <= '0';   
                 empty_right <= '0'; 
         else
-                empty_left <= '1';   
-                empty_right <= '1';   
+                empty_right <= '1';
         end if;
+        
     end if;    
 
 end process;
